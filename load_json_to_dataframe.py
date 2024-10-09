1f12050155eb1e66d027b2955fd8f8b2d2f60dca
dargasova <dianaargasova@gmail.com>
2024-10-09 17:10:53 +0300

    Финальный коммит

--- load_json_to_dataframe.py
+++ load_json_to_dataframe.py
@@ -7,6 +7,7 @@ from prettytable import PrettyTable
 
 
 def load_json_to_dataframe(file_path):
+
     with open(file_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
@@ -16,51 +17,32 @@ def load_json_to_dataframe(file_path):
 
 
 def get_normalized_brands(df):
-    if 'Бренд' not in df.columns:
-        raise ValueError("Столбец 'Бренд' отсутствует в DataFrame")
-
-    most_common_brand = df['Бренд'].mode()[0] if not df['Бренд'].mode().empty else None
-
+    most_common_brand = df['Бренд'].mode()[0]
     df['Бренд'] = df['Бренд'].fillna(most_common_brand)
-
     normalized_brands = df['Бренд'].drop_duplicates().str.title().tolist()
 
     return normalized_brands
 
 
 def filter_above_average_price(df):
-    if 'Финальная цена' not in df.columns:
-        raise ValueError("Столбец 'Финальная цена' отсутствует в DataFrame")
-
     df['Финальная цена'] = pd.to_numeric(df['Финальная цена'], errors='coerce')
-
     average_price = df['Финальная цена'].mean()
-
     df_filtered = df.loc[df['Финальная цена'] > average_price]
 
     return df_filtered, average_price
 
 
 def apply_discount_mask(df):
-    if 'Старая цена' not in df.columns or 'Финальная цена' not in df.columns:
-        raise ValueError("Отсутствуют столбцы 'Старая цена' или 'Финальная цена' в DataFrame")
-
     df['Старая цена'] = pd.to_numeric(df['Старая цена'], errors='coerce')
     df['Финальная цена'] = pd.to_numeric(df['Финальная цена'], errors='coerce')
-
     discount_mask = df['Старая цена'] > df['Финальная цена']
-
     df_discounted = df.loc[discount_mask]
 
     return df_discounted
 
 
 def combine_name_and_price(df):
-    if 'Название' not in df.columns or 'Финальная цена' not in df.columns:
-        raise ValueError("Отсутствуют столбцы 'Название' или 'Финальная цена' в DataFrame.")
-
     df_valid = df.dropna(subset=['Название', 'Финальная цена']).copy()
-
     df_valid['Название и цена'] = df_valid['Название'] + ' - ' + df_valid['Финальная цена'].astype(str)
 
     return df_valid[['Название и цена']]
